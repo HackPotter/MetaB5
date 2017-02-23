b063c72e8842a31a14f15081288d57643021eb3b
Bradr555 <bradr555@gmail.com>
2017-02-23 14:32:45 -0600

    Removed MeshBaker scripts because they are unused

--- Assets/Scripts/AI/AgentBehavior.cs
+++ Assets/Scripts/AI/AgentBehavior.cs
@@ -1,8 +1,7 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class AgentBehavior : MonoBehaviour
-{
+public class AgentBehavior : MonoBehaviour {
 #pragma warning disable 0067, 0649
     [SerializeField]
     private Agent _agent;
@@ -10,24 +9,20 @@ public class AgentBehavior : MonoBehaviour
 
     private List<Steering> _allSteerings = new List<Steering>();
 
-    public Vector3 GetAcceleration()
-    {
+    public Vector3 GetAcceleration() {
         Vector3 acceleration = Vector3.zero;
-        foreach (Steering steering in _allSteerings)
-        {
+        foreach (Steering steering in _allSteerings) {
             acceleration += steering.GetAcceleration() * steering.Weight / _allSteerings.Count;
         }
         return acceleration;
     }
 
-    public void RegisterSteering(Steering steering)
-    {
+    public void RegisterSteering(Steering steering) {
         _allSteerings.Add(steering);
         steering.Agent = _agent;
     }
 
-    public void DeregisterSteering(Steering steering)
-    {
+    public void DeregisterSteering(Steering steering) {
         _allSteerings.Remove(steering);
     }
 }
--- Assets/Scripts/AI/CameraChange.cs
+++ Assets/Scripts/AI/CameraChange.cs
@@ -1,48 +1,40 @@
 using UnityEngine;
 
-public class CameraChange : MonoBehaviour
-{
+public class CameraChange : MonoBehaviour {
     private bool main;
     public Camera ship;
     public Camera pro;
 
-    void Awake()
-    {
+    void Awake() {
         main = true;
     }
 
-    public void SetShipCam()
-    {
+    public void SetShipCam() {
         main = true;
         ship.GetComponent<Camera>().enabled = true;
         pro.GetComponent<Camera>().enabled = false;
     }
 
-    public void SetProCam()
-    {
+    public void SetProCam() {
         main = false;
         ship.GetComponent<Camera>().enabled = false;
         pro.GetComponent<Camera>().enabled = true;
     }
 
-    bool IsShipCamActive()
-    {
+    bool IsShipCamActive() {
         return main;
     }
 
-    void SwitchCamera()
-    {
+    void SwitchCamera() {
         main = !main;
 
-        if (main)
-        {
+        if (main) {
             //set to main
             ship.GetComponent<Camera>().enabled = true;
             pro.GetComponent<Camera>().enabled = false;
             print("main");
         }
-        else
-        {
+        else {
             //set to other
             ship.GetComponent<Camera>().enabled = false;
             pro.GetComponent<Camera>().enabled = true;
