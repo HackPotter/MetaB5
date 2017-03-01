d892dc1ec9385ecd3c166db893d563ef81892df4
Bradr555 <bradr555@gmail.com>
2017-03-01 16:23:45 -0600

    Commented out seemingly unused section of code

--- Assets/Scripts/Views/UnityViews/Biolog/NewBiologUI.cs
+++ Assets/Scripts/Views/UnityViews/Biolog/NewBiologUI.cs
@@ -4,10 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Metablast.UI
-{
-    public class NewBiologUI : UIScreen, IBiologView
-    {
+namespace Metablast.UI {
+    public class NewBiologUI : UIScreen, IBiologView {
 #pragma warning disable 0067, 0649
         [SerializeField]
         private AnimationClip _showAnimation;
@@ -33,8 +31,7 @@ namespace Metablast.UI
 
         private List<BiologEntryListElement> _currentListElements = new List<BiologEntryListElement>();
 
-        void Start()
-        {
+        void Start() {
             _showAnimation.SampleAnimation(gameObject, 0f);
 
             BuildEntryList();
@@ -43,23 +40,18 @@ namespace Metablast.UI
             _searchField.onValueChanged.AddListener(searchFieldChangedHandler);
         }
 
-        void searchFieldChangedHandler(string searchString)
-        {
-            for (int i = 0; i < _currentListElements.Count; i++)
-            {
+        void searchFieldChangedHandler(string searchString) {
+            for (int i = 0; i < _currentListElements.Count; i++) {
                 BiologEntryListElement listElement = _currentListElements[i];
 
-                if (listElement.Entry.EntryName.ToLower().Contains(searchString.Trim().ToLower()))
-                {
+                if (listElement.Entry.EntryName.ToLower().Contains(searchString.Trim().ToLower())) {
                     listElement.gameObject.SetActive(true);
                     continue;
                 }
 
                 bool matches = false;
-                foreach (string tag in listElement.Entry.Tags)
-                {
-                    if (tag.Trim().ToLower().Contains(searchString.Trim().ToLower()))
-                    {
+                foreach (string tag in listElement.Entry.Tags) {
+                    if (tag.Trim().ToLower().Contains(searchString.Trim().ToLower())) {
                         matches = true;
                         break;
                     }
@@ -68,39 +60,31 @@ namespace Metablast.UI
             }
         }
 
-        void ExitButtonOnClickHandler()
-        {
-            if (ExitButtonPressed != null)
-            {
+        void ExitButtonOnClickHandler() {
+            if (ExitButtonPressed != null) {
                 ExitButtonPressed();
             }
         }
 
-        void OnElementClick(BiologEntryListElement entryElement)
-        {
+        void OnElementClick(BiologEntryListElement entryElement) {
             SetBiologEntry(entryElement.Entry);
             _buttonPressed.Invoke(entryElement);
         }
 
-        public void SetBiologEntry(BiologEntry entry)
-        {
+        public void SetBiologEntry(BiologEntry entry) {
             _databaseText.text = entry.DescriptionText;
         }
 
-        public void RefreshEntries()
-        {
-            for (int i = 0; i < _currentListElements.Count; i++)
-            {
+        public void RefreshEntries() {
+            for (int i = 0; i < _currentListElements.Count; i++) {
                 GameObject.Destroy(_currentListElements[i].gameObject);
             }
             _currentListElements.Clear();
             BuildEntryList();
         }
 
-        private void BuildEntryList()
-        {
-            foreach (BiologEntry entry in GameContext.Instance.Player.BiologProgress.UnlockedEntries)
-            {
+        private void BuildEntryList() {
+            foreach (BiologEntry entry in GameContext.Instance.Player.BiologProgress.UnlockedEntries) {
                 BiologEntryListElement entryElement = (BiologEntryListElement)GameObject.Instantiate(_listElementPrefab, _databaseList.transform.position, Quaternion.identity);
                 entryElement.transform.SetParent(_databaseList.transform);
                 entryElement.transform.localScale = Vector3.one;
@@ -112,55 +96,46 @@ namespace Metablast.UI
 
         public event Action ExitButtonPressed;
 
-        public bool IsShowing
-        {
+        public bool IsShowing {
             get { return _isShowing; }
         }
 
-        public void SetTint(float tint)
-        {
-            Color newColor = _tintImage.color;
-            newColor.a = tint;
-            _tintImage.color = newColor;
+        public void SetTint(float tint) {
+            // Brad 3/1/17
+            //Color newColor = _tintImage.color;
+            //newColor.a = tint;
+            //_tintImage.color = newColor;
         }
 
-        public override void Show(Action onComplete, float animationTime = 0.4f)
-        {
+        public override void Show(Action onComplete, float animationTime = 0.4f) {
             _animation.clip = _showAnimation;
             _animation[_showAnimation.name].speed = 1f;
-            StartCoroutine(PlayAnimationCoroutine(_showAnimation, () =>
-            {
+            StartCoroutine(PlayAnimationCoroutine(_showAnimation, () => {
                 _isShowing = true;
                 if (onComplete != null) onComplete();
             }));
         }
 
-        public override void Hide(Action onComplete, float animationTime = 0.4f)
-        {
+        public override void Hide(Action onComplete, float animationTime = 0.4f) {
             _animation.clip = _showAnimation;
-            if (!_animation.isPlaying)
-            {
+            if (!_animation.isPlaying) {
                 _animation[_showAnimation.name].time = _showAnimation.length;
             }
 
             _animation[_showAnimation.name].speed = -1f;
-            StartCoroutine(PlayAnimationCoroutine(_showAnimation, () =>
-            {
+            StartCoroutine(PlayAnimationCoroutine(_showAnimation, () => {
                 _isShowing = false;
                 if (onComplete != null) onComplete();
             }));
         }
 
-        private IEnumerator PlayAnimationCoroutine(AnimationClip animationClip, Action onComplete)
-        {
+        private IEnumerator PlayAnimationCoroutine(AnimationClip animationClip, Action onComplete) {
             _animation.Play(animationClip.name, PlayMode.StopAll);
-            while (_animation.isPlaying)
-            {
+            while (_animation.isPlaying) {
                 yield return null;
             }
 
-            if (onComplete != null)
-            {
+            if (onComplete != null) {
                 onComplete();
             }
         }
