d56ae87a8aea2b6f36b47ffc2584cb849dc47af6
Bradr555 <bradr555@gmail.com>
2017-03-01 15:47:21 -0600

    Removed empty lambda

--- Assets/Scripts/Triggers/EventResponders/UI/SetHUDVisibility.cs
+++ Assets/Scripts/Triggers/EventResponders/UI/SetHUDVisibility.cs
@@ -1,8 +1,7 @@
 ﻿using UnityEngine;
 
 [Trigger(DisplayPath = "UI")]
-public class SetHUDVisibility : EventResponder
-{
+public class SetHUDVisibility : EventResponder {
 #pragma warning disable 0067, 0649
     [SerializeField]
     private bool _visible;
@@ -12,32 +11,18 @@ public class SetHUDVisibility : EventResponder
     private float _animationTime;
 #pragma warning restore 0067, 0649
 
-    public override void OnEvent(ExecutionContext context)
-    {
-        if (_visible)
-        {
-			if  (MetablastUI.Instance.BiologView.IsShowing)
-			{
-				// do nothing because we don't want to cover up the biolog.
-			}
-            else if (_overrideDefaultAnimationTime)
-            {
+    public override void OnEvent(ExecutionContext context) {
+        if (_visible) {
+            if (MetablastUI.Instance.BiologView.IsShowing) {
+                // do nothing because we don't want to cover up the biolog.
+            }
+            else if (_overrideDefaultAnimationTime) {
                 MetablastUI.Instance.HudView.Show(() => { }, _animationTime);
             }
-            else
-            {
-                MetablastUI.Instance.HudView.Show(() => { });
-            }
         }
-        else
-        {
-            if (_overrideDefaultAnimationTime)
-            {
+        else {
+            if (_overrideDefaultAnimationTime) {
                 MetablastUI.Instance.HudView.Hide(() => { }, _animationTime);
-            }
-            else
-            {
-                MetablastUI.Instance.HudView.Hide(() => { });
             }
         }
     }
