f8a31ce0ec5a14ef71910593f3bdbcf7b493288c
Bradr555 <bradr555@gmail.com>
2017-03-27 19:52:11 -0500

    Created Pause script for easy pause/unpause implementation. Also updated UI buttons to use the new Pause script.

---
+++ Assets/Scripts/Triggers/EventResponders/Gameplay/Pause.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    public void PauseGame()
+    {
+        GameState.Instance.PauseLevel = PauseLevel.Menu;
+    }
+    public void UnPauseGame()
+    {
+        GameState.Instance.PauseLevel = PauseLevel.Unpaused;
+    }
+}
