c2864a67ef751450bef1e950f67dffee9fb116b7
j-potter <jpotter@iastate.edu>
2017-05-12 16:27:48 -0500

    Added Credit Scene that Actually Works

---
+++ Assets/Scripts/Animation/Credits.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour {
+
+    public GameObject camera;
+    public int speed = 1;
+    public string level;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        camera.transform.Translate(Vector3.down * Time.deltaTime * speed);
+
+        StartCoroutine(waitFor());
+	}
+
+    IEnumerator waitFor()
+    {
+        yield return new WaitForSeconds(107);
+        Application.LoadLevel(level);
+    }
+}
