8ce7f3eb9f47c1b786a25c4c3fa003870098d4e5
j-potter <jpotter@iastate.edu>
2017-05-25 16:19:30 -0500

    Added top billing credits (directors, etc) to cool fading credits

---
+++ Assets/CreditFading.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CreditFading : MonoBehaviour
+{
+#pragma warning disable 0067, 0649
+    [SerializeField]
+    private GameObject[] _gameObjects;
+
+    [SerializeField]
+    private int[] _timings;
+#pragma warning restore 0067, 0649
+
+    void Start ()
+    {
+        StartCoroutine(Fade());
+    }
+
+    void Update ()
+    {
+
+    }
+
+    IEnumerator Fade()
+    {
+        for(int i = 0; i < _gameObjects.Length; i++)
+        {
+            _gameObjects[i].SetActive(true);
+            yield return new WaitForSecondsRealtime(_timings[i]);
+            _gameObjects[i].SetActive(false); 
+        }
+    }
+}
+
+
